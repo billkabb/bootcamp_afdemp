7571cab427b0fc7bd9b1255045f0a3c958ef376e
billkabb <billkabb@gmail.com>
2016-10-17 00:26:29 +0300

    remove test

--- test/myQuery.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO mydb.students (idstudents,fnamest,lnamest,datest)
-VALUES (1,"Anasrtasia","Griva","1990-12-10"),
-	   (2,"Vasilis","Kabbadias","1974-05-22")
-       
-select * from mydb.students
